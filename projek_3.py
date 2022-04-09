32da9d4aa0029276ff619a22a6ebcf50b50871a8
Annisa Lathifa <102348322+lthfaannisa@users.noreply.github.com>
2022-04-09 20:54:18 +0700

    Update projek_3.py

--- projek_3.py
+++ projek_3.py
@@ -1,1 +1,11 @@
 #soal 3
+print("Projek nomor 3 Kelompok 5\n")
+n = int(input("Masukkan baris yang anda inginkan :  "))
+
+for x in range(n + 1):
+    for y in range(n - x + 1):
+        print(" ", end="")
+    for z in range(x):
+        print("", z + 1, end="")
+
+    print()
