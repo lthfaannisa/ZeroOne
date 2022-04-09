c0aad089336c886957411f3ed11d317867b782c0
Annisa Lathifa <102348322+lthfaannisa@users.noreply.github.com>
2022-04-09 20:52:40 +0700

    Update projek_4.py

--- projek_4.py
+++ projek_4.py
@@ -1,1 +1,11 @@
 #soal 4
+print("Projek nomor 4 Kelompok 5\n")
+
+angka = [11,4,5,2,5,6,93,29]
+
+print("Daftar angka sebelum disorting :")
+print(angka)
+
+print("Daftar angka sesudah disorting :")
+angka.sort()
+print(angka)
