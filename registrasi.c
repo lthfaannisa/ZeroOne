4a16658d9b43b2ab91782ea0355f16b1fd0d2cb1
nabilaaprillia <102314180+nabilaaprillia@users.noreply.github.com>
2022-05-07 23:39:47 +0700

    Update registrasi.c

--- registrasi.c
+++ registrasi.c
@@ -1,1 +1,16 @@
 //pembuatan file bin
+
+
+
+
+  FILE *fpw = fopen("database/login.bin","wb");
+  char login[20] = "ZeroOne@root";
+/*disini mau buat username ZeroOne
+ passwordnya root dan @ sebagai pemisah antara username dan pass*/
+
+  fwrite(login, sizeof(char), sizeof(login)/sizeof(char), fpw);
+
+  fclose(fpw);
+
+  return 0:
+}
