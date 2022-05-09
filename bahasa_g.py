097a409264b2d55496aeb6d538706d167df6a2f0
LailaAsrin <101389349+LailaAsrin@users.noreply.github.com>
2022-05-09 09:32:00 +0700

    Update bahasa_g.py

--- bahasa_g.py
+++ bahasa_g.py
@@ -1,1 +1,5 @@
-#bahasa g
+kalimat = input('input kalimat :')
+
+res = kalimat.replace('a', "aga").replace('i', "igi").replace('u', "ugu").replace('e', "ege").replace('o', "ogo").replace('A', "Aga").replace('I', "Igi").replace('U', "Ugu").replace('O', "Ogo").replace('E', "Ege")
+
+print("Output bahasa g : " + res)
