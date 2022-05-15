c3b4ec302b5bf6e7cabda04aa5df9afb1e47a0c3
Annisa Lathifa <102348322+lthfaannisa@users.noreply.github.com>
2022-05-15 21:02:12 +0700

    Update pola_segitiga.py

--- pola_segitiga.py
+++ pola_segitiga.py
@@ -1,12 +1,11 @@
 #pola segitiga
-print("Projek nomor 3 Kelompok 5/n")
-n = int(input("Masukkan baris yang anda inginkan : "))
+print("Projek nomor 3 Kelompok 5")
+n = int(input("Masukkan baris yang anda inginkan :  "))
 
-for x in range(n+1):
-  for y in range(n - x +1):
-    print(" ",end="")
+for x in range(n + 1):
+    for y in range(n - x + 1):
+        print("", end=" ")
     for z in range(x):
-      print("", z +1,end="")
-      
-      print()
-      
+        print("", z + 1, end="")
+
+    print()
