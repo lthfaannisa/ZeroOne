ccf59e071726616525957fc0f738a9a4765395fe
Askar Aziz <103616025+askaraziz@users.noreply.github.com>
2022-05-15 20:43:14 +0700

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,4 +1,11 @@
+print("Projek nomor 4 Kelompok 5\n")
 
+angka = [11,4,5,2,5,6,93,29]
+
+print("Daftar angka sebelum disorting :")
+print(angka)
+
+print("Daftar angka sesudah disorting : ")
 
 //sultan//
 angka.sort()
